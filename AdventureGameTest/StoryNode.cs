a6ae43c1a4f15443e09a66e044bd955891593f5a
Tim Freese <freese.tim@gmail.com>
2020-09-14 21:30:37 -0500

    Add project files.

---
+++ AdventureGameTest/StoryNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace AdventureGameTest
+{
+	public class StoryNode
+	{
+		public int StoryNodeId { get; set; }
+
+		public List<StoryNode> Parent { get; set; }
+
+		public List<StoryNode> Children { get; set; }
+
+		public bool Visited { get; set; }
+
+		//people
+
+		//goal
+	}
+}
